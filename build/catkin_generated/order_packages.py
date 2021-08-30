1e9010436fdfb36272e6c1aad7715092f41b37ad
enhao <howardchung29@gmail.com>
2021-08-30 15:34:56 +0800

    for lesson two add C_one.cpp use git pull

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/hao/test/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/hao/test/devel;/opt/ros/melodic'.split(';') if '/home/hao/test/devel;/opt/ros/melodic' != '' else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,9 +7,6 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/hao/test/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/hao/test/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/hao/test/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH='/opt/ros/melodic/lib'
+export PKG_CONFIG_PATH='/opt/ros/melodic/lib/pkgconfig'
 export PWD='/home/hao/test/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/hao/test/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/hao/test/src:$ROS_PACKAGE_PATH"
